1b67b9551ae1dc81ddef5a255c858c208a88f98d
Timea <timea.reich@gmail.com>
2022-10-07 00:28:51 +0100

    add main section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Navigation from "./Components/Navigation";
 import Hero from "./Components/Hero";
 import Footer from "./Components/Footer";
+import MainSection from "./Components/MainSection";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Hero />
+      <MainSection />
       <Footer />
     </div>
   );
