492a58e73954b96311aa6f5205906fc7520f6fea
Timea <timea.reich@gmail.com>
2022-10-06 23:55:57 +0100

    add footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navigation from "./Components/Navigation";
 import Hero from "./Components/Hero";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Hero />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <div className="footer-div">
+      <p>2022 ViteAcademia</p>
+      <p>All Rights Reserved</p>
+
+      <ul>
+        <li>Privacy </li>
+        <li>Terms</li>
+      </ul>
+    </div>
+  );
+};
+export default Footer;
