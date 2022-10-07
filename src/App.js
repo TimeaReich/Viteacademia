d4fcbed3e5bad03111dbd63529e62bb3b16daca3
Timea <timea.reich@gmail.com>
2022-10-07 23:02:29 +0100

    test different layout

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Navigation from "./Components/Navigation";
 import Hero from "./Components/Hero";
 import Footer from "./Components/Footer";
 import MainSection from "./Components/MainSection";
+import Test from "./Components/Test";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Hero />
-      <MainSection />
+      {/* <MainSection /> */}
+      <Test />
       <Footer />
     </div>
   );
