777f5a741c47a06d7c91cab977c3f82840bdbc3d
Timea <timea.reich@gmail.com>
2022-10-08 15:44:34 +0100

    trying different layout

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import Navigation from "./Components/Navigation";
 import Hero from "./Components/Hero";
 import Footer from "./Components/Footer";
 import MainSection from "./Components/MainSection";
-import Test from "./Components/Test";
+import Blob from "./Blob";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Hero />
-      {/* <MainSection /> */}
-      <Test />
+      <MainSection />
+      <Blob />
+
       <Footer />
     </div>
   );
---
+++ src/Blob.js
@@ -0,0 +1,9 @@
+const Blob = () => {
+  return (
+    <div className="main">
+      <div className="blob"></div>
+      <p className="text">Health</p>
+    </div>
+  );
+};
+export default Blob;
