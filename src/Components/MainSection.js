4f987004fb837fa805379b4354c075eb35794725
Timea <timea.reich@gmail.com>
2022-10-12 22:31:38 +0100

    styling with grid

--- src/Components/MainSection.js
+++ src/Components/MainSection.js
@@ -7,27 +7,27 @@ const MainSection = () => {
     <div className="main-section-container">
       <div className="main-section-div">
         <Blob text="Health" className="blob-health" />
-        <p className="main-section-content">
-          ViteAcademia provides online courses and coaching sessions to improve
-          your health and wellbeing.
-        </p>
       </div>
       <div className="main-section-div">
         <Blob text="Health Nutrition Course" className="blob-health-n" />
-        <p className="main-section-content">
-          There are no good or bad foods but the relative amounts to be consumed
-          play a key role in our health. You can develop a deeper understanding
-          of the food and implement lasting changes that improve your energy,
-          balance and health…
-        </p>
       </div>
       <div className="main-section-div">
         <Blob text="Health Coach" className="blob-health-coach" />
-        <p className="main-section-content">
-          ViteAcademia's health coach develops a personalised program for your
-          better health physically and mentally.
-        </p>
       </div>
+      <p className="main-section-content">
+        ViteAcademia provides online courses and coaching sessions to improve
+        your health and wellbeing.
+      </p>
+      <p className="main-section-content">
+        There are no good or bad foods but the relative amounts to be consumed
+        play a key role in our health. You can develop a deeper understanding of
+        the food and implement lasting changes that improve your energy, balance
+        and health…
+      </p>
+      <p className="main-section-content">
+        ViteAcademia's health coach develops a personalised program for your
+        better health physically and mentally.
+      </p>
     </div>
   );
 };
