7c1b76147e6af27a6e83b3f20796dad64277e31c
Timea <timea.reich@gmail.com>
2022-10-14 20:10:05 +0100

    footer icons

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,24 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGraduationCap } from "@fortawesome/free-solid-svg-icons";
+import { faUserGraduate } from "@fortawesome/free-solid-svg-icons";
+import { faUtensils } from "@fortawesome/free-solid-svg-icons";
+
 const Footer = () => {
   return (
     <div className="footer-div">
       <p>2022 ViteAcademia</p>
       <p>All Rights Reserved</p>
-
+      <ul>
+        <li>
+          <FontAwesomeIcon icon={faGraduationCap} size={"2x"} />
+        </li>
+        <li>
+          <FontAwesomeIcon icon={faUserGraduate} size={"2x"} />
+        </li>
+        <li>
+          <FontAwesomeIcon icon={faUtensils} size={"2x"} />
+        </li>
+      </ul>
       <ul>
         <li>Privacy </li>
         <li>Terms</li>
